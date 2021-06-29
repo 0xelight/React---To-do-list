bc40e6526ce128ead95119e3f822b1c7abe4879d
Djehouty <dorianquenot@gmail.com>
2021-06-29 22:49:07 +0200

    dynamic datas

---
+++ src/initialData.js
@@ -0,0 +1,8 @@
+let initialData = [
+    {id: 'jserp02', name: 'Ranger la vaisselle', completed: false},
+    {id: 'jserp03', name: 'Ranger les tiroirs', completed: false},
+    {id: 'jserp04', name: 'Ranger les papiers', completed: false},
+    {id: 'jserp05', name: 'mourir', completed: false},
+]
+    
+export default initialData;
