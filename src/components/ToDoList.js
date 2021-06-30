58f4dc45bce56c7247abea07196922453ecc5624
Djehouty <dorianquenot@gmail.com>
2021-06-30 13:39:59 +0200

    finish

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,16 +1,43 @@
 import React from  'react'
 import ToDo from './ToDo'
 
-const TodoList = ({tasks}) => (
+const TodoList = ({tasks, match, onToggleCompleted}) => {
+    let filteredTasks
+
+    switch (match.params.filter) {
+        case 'completed':
+            filteredTasks = tasks.filter(task => task.completed)
+        break;
+        default:
+            filteredTasks = tasks
+    }
+
+    if(filteredTasks.length === 0){
+        return(
+            <React.Fragment>
+                <h1>Voici la liste</h1>
+                <ul className="list-group m-3">
+                    <li className="list-group-item">Aucune tâche à afficher</li>
+                </ul>
+            </React.Fragment>  
+            
+        
+        ) 
+    }else{
+       return(
     <React.Fragment>
         <h1>Voici la liste</h1>
         <ul className="list-group m-3">
             {
-                tasks.map((task) => <ToDo task={task} key={task.id}/>)
+                filteredTasks.map((task) => <ToDo task={task} key={task.id} onToggleCompleted={onToggleCompleted}/>)
             }
         </ul>
     </React.Fragment>  
     
 
-)
+)   
+    }
+  
+}
+
 export default TodoList
